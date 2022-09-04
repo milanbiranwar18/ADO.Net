bfecf0f39b4df1affb94e3fba158f5f9ebd754e3
Milan Biranwar <milanbiranwar06@gmail.com>
2022-09-04 14:51:17 +0530

    Adding program for Employee data to the database table

--- Program.cs
+++ Program.cs
@@ -4,3 +4,19 @@ using EmployeePayroll;
 Console.WriteLine("Hello, World!");
 
 Console.WriteLine("Wellcome to the EmployeePayroll porblem in ADO.Net");
+EmployeeRepo repo = new EmployeeRepo();
+
+EmployeeModel employeeModel = new EmployeeModel();
+employeeModel.EmployeeName = "Bruce Wayne";
+employeeModel.PhoneNumber = "54894516846";
+employeeModel.Address = "Banglore";
+employeeModel.Department = "Hr";
+employeeModel.Gender = 'M';
+employeeModel.BasicPay = 323400.00;
+employeeModel.Deductions = 2500.00;
+employeeModel.TaxablePay = 200.00;
+employeeModel.NetPay = 25000.00;
+employeeModel.City = "Banglore";
+employeeModel.Country = "India";
+//employeeModel.StartDate = DateTime.Now;
+repo.AddEmployee(employeeModel);
