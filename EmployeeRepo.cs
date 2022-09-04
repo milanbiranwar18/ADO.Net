43d9b368c533a1fab1ef2e38b7ac3d7ac2700f92
Milan Biranwar <milanbiranwar06@gmail.com>
2022-09-04 14:49:55 +0530

    Program for Adding Employee to the Database in EmployeePayroll table

---
+++ EmployeeRepo.cs
@@ -0,0 +1,60 @@
+﻿using MongoDB.Driver.Core.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeePayroll
+{
+    public class EmployeeRepo
+    {
+        public static string  connectionstring = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=PayrollService1;Integrated Security=True";
+        SqlConnection connection = new SqlConnection(connectionstring);
+
+        public bool AddEmployee(EmployeeModel employeeModel)
+        {
+            try
+            {
+                using (this.connection)
+                {
+                    SqlCommand command = new SqlCommand("SpAddEmployeeDetails", this.connection);
+                    command.CommandType = System.Data.CommandType.StoredProcedure;
+                    command.Parameters.AddWithValue("@EmployeeName", employeeModel.EmployeeName);
+                    command.Parameters.AddWithValue("@EmployeeID", employeeModel.EmployeeID);
+                    command.Parameters.AddWithValue("@PhoneNumber", employeeModel.PhoneNumber);
+                    command.Parameters.AddWithValue("@Address", employeeModel.Address);
+                    command.Parameters.AddWithValue("@Department", employeeModel.Department);
+                    command.Parameters.AddWithValue("@Gender", employeeModel.Gender);
+                    command.Parameters.AddWithValue("@Deductions", employeeModel.Deductions);
+                    command.Parameters.AddWithValue("@TaxablePay", employeeModel.TaxablePay);
+                    command.Parameters.AddWithValue("@NetPay", employeeModel.NetPay);
+                    command.Parameters.AddWithValue("@StartDate", employeeModel.StartDate);
+                    command.Parameters.AddWithValue("@City", employeeModel.City);
+                    command.Parameters.AddWithValue("@Country", employeeModel.Country);
+                   this.connection.Open();
+                    var result = command.ExecuteNonQuery();
+                    this.connection.Close();
+                    if(result != 0)
+                    {
+                        return true;
+                    }
+                    return false;
+
+                }
+            }
+            catch (Exception e)
+            {
+                // throw new Exception(e.Message);
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                this.connection.Close();
+            }
+            return false;
+        }
+
+    }
+}
