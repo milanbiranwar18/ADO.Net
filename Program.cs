00b48476213c1ad5691a773a24b63148bb2ef52c
Milan Biranwar <milanbiranwar06@gmail.com>
2022-09-04 14:42:15 +0530

    Greeting Message

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using EmployeePayroll;
+
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Wellcome to the EmployeePayroll porblem in ADO.Net");
